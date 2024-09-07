afe5f09c4d52fb5889370d552b4d5cc7afbc457b
pcsosa <pcsosa.pcs@fpuna.edu.py>
2024-09-07 19:28:44 -0400

    angular test pablo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component'; // Ajusta la ruta según la ubicación del componente
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PrincipalComponent }, // Ruta raíz
+  { path: 'principal', component: PrincipalComponent }, // Ruta '/principal'
+];
